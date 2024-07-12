8fdcdb6c3fc8819961668ce09fbad8f0615608cd
IIpapuII <wnserranoa@unadvirtual.edu.co>
2024-07-12 10:24:08 -0500

    se genera la estrucutura de los dos primeros modulos explicados por la documentación de django

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Esta es la prueba del funcionamiento del index")
+def details(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
